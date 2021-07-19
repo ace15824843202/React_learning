ff17539a9521f5d4c5a9f7981382c71d297e2359
GuanTing <1305945901@qq.com>
2021-07-19 16:25:05 +0800

    单个代理配置
    单个代理配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 class App extends Component {
+    getData = ()=>{//代理服务器是开在前端的端口，（跨域是发送成功，数据返回被拦截）
+        axios.get('http://localhost:3000/students').then(res=>{
+
+            console.log('students',res)
+        },error=>{})
+    }
+    getCar = ()=>{
+        // axios.get('http://localhost:3000/api2/cars').then(res=>{
+        //
+        //     console.log('cars',res)
+        // },error=>{})
+    }
     render() {
         return (
             <div>
-                。。。app
+             <button onClick={this.getData}>点我获取学生数据</button>
+             <button onClick={this.getCar}>点我获取汽车数据</button>
             </div>
         );
     }

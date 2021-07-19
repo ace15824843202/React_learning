47a2d68fd69b05e9e12d5269fed46d0a82608970
GuanTing <1305945901@qq.com>
2021-07-19 17:39:22 +0800

    配置多个代理
    配置多个代理

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import axios from 'axios'
 
 class App extends Component {
     getData = ()=>{//代理服务器是开在前端的端口，（跨域是发送成功，数据返回被拦截）
-        axios.get('http://localhost:3000/students').then(res=>{
+        axios.get('http://localhost:3000/api1/students').then(res=>{
 
             console.log('students',res)
         },error=>{})
     }
     getCar = ()=>{
-        // axios.get('http://localhost:3000/api2/cars').then(res=>{
-        //
-        //     console.log('cars',res)
-        // },error=>{})
+        axios.get('http://localhost:3000/api2/cars').then(res=>{
+
+            console.log('cars',res)
+        },error=>{})
     }
     render() {
         return (

9c56047f6e348041da1a0182da2bcc6ee60dbe3f
GuanTing <1305945901@qq.com>
2021-08-20 18:51:38 +0800

    路由简单使用react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
 //React.StrictMode 检查代码里不合理的位置
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 

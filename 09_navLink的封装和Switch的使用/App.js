460c2a7e1d32926e332ce5cd2f793d9af6b4bdf5
GuanTing <1305945901@qq.com>
2021-08-26 15:02:20 +0800

    解决bootstrap文件加载的路径问题

--- 09_navLink的封装和Switch的使用/App.js
+++ 09_navLink的封装和Switch的使用/App.js
@@ -3,17 +3,32 @@ import {Switch, Route} from 'react-router-dom'
 //路由页面组件 没有传props也能收到props里面有{history,match,location}
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Test from "./pages/Test"
 //一般组件
 import Header from "./components/Header"
 import MyNavLink from "./components/MyNavLink";
 
 /*
-解决多级路径刷新页面样式丢失的问题
-例如：<Route path="/test/about" component={About}/>
-* 1.public/index.html 中 引入样式时不写（./和../）写/（常用）
-* 2.public/index.html 中 引入样式时不写（./和../）写%PUBLIC_URL%（常用，只能在react脚手架里用）
-* 3.使用hashRouter #后面的都不会带给服务器
-* */
+navLink与NavLink封装
+    1.NavLink可以实现路由链接高亮，通过activeClassName设置样式
+    2.标签体内容是一个特殊的标签属性
+    3.通过this.props.children可以获取标签体内容
+
+ */
+
+/*
+Switch的使用
+    1.通常情况下，path和component是一一对应的关系
+    2.如果path对应多个组件，匹配到了第一个，也会继续向下匹配，展示多个组件。效率低下
+         <Route path="/about" component={About}/>
+         <Route path="/home" component={Home}/>
+          <Route path="/aba" component={aba}/>
+          <Route path="/home" component={Test}/>
+
+    3.Switch可以提高路由匹配的效率（单一匹配）,匹配到了就不会继续向下匹配
+
+ */
+
 
 class App extends Component {
 
@@ -32,8 +47,8 @@ class App extends Component {
                         <div className="list-group">
                             {/*编写路由链接*/}
                             {/*NavLink有高亮效果，而Link没有，可以添加activeClassName*/}
-                            <MyNavLink to="/test/about">About</MyNavLink>
-                            <MyNavLink to="/test/home">Home</MyNavLink>
+                            <MyNavLink to="/about">About</MyNavLink>
+                            <MyNavLink to="/home">Home</MyNavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
@@ -41,8 +56,9 @@ class App extends Component {
                             <div className="panel-body">
                                 {/*注册路由*/}
                                 <Switch>
-                                    <Route path="/test/about" component={About}/>
-                                    <Route path="/test/home" component={Home}/>
+                                    <Route path="/about" component={About}/>
+                                    <Route path="/home" component={Home}/>
+                                    <Route path="/home" component={Test}/>
                                 </Switch>
                             </div>
                         </div>
